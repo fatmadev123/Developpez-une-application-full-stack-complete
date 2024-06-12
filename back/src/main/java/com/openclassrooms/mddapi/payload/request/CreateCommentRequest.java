b88c590d8a3acc364d8c83909a64da6786437474
Fatma Dev <devfatma4@gmail.com>
2024-06-12 08:52:32 +0200

    adding a request module

---
+++ back/src/main/java/com/openclassrooms/mddapi/payload/request/CreateCommentRequest.java
@@ -0,0 +1,14 @@
+package com.openclassrooms.mddapi.payload.request;
+
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+public class CreateCommentRequest {
+    @NotNull
+    @Size(max = 2000)
+    private String comment;
+}
