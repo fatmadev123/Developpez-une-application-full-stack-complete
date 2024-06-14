38f413a806ccd26871e4a56c21565a682a887e3f
Fatma Dev <devfatma4@gmail.com>
2024-06-14 16:09:09 +0200

    creation a guard for unauthentication

---
+++ front/src/app/guards/unauth.guard.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+
+@Injectable({ providedIn: 'root' })
+export class UnauthGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  public canActivate(): boolean {
+    if (localStorage.getItem('token')) {
+      this.router.navigateByUrl('/post');
+      return false;
+    }
+    return true;
+  }
+}
