c18ea336d9d5022f37b22399f61cecb435658630
Fatma Dev <devfatma4@gmail.com>
2024-06-14 15:57:28 +0200

    Define an authentication guard

---
+++ front/src/app/guards/auth.guard.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  public canActivate(): boolean {
+    if (localStorage.getItem('token')) {
+      return true;
+    }
+    this.router.navigateByUrl('/login');
+    return false;
+  }
+}
