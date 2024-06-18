9c0dce90c0cad44b4c5a1b249836c59f8c94cfb8
Fatma Dev <devfatma4@gmail.com>
2024-06-18 16:15:35 +0200

    Updating routes in file app-routing.ts

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -4,6 +4,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { UnauthGuard } from './guards/unauth.guard';
+import { PostComponent } from './pages/post/post.component';
+import { AuthGuard } from './guards/auth.guard';
+import { TopicComponent } from './pages/topic/topic.component';
+import { UserComponent } from './pages/user/user.component';
+import { CreatePostComponent } from './pages/create-post/create-post.component';
+import { PostDetailComponent } from './pages/post-detail/post-detail.component';
 
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
@@ -14,6 +20,19 @@ const routes: Routes = [
     path: 'register',
     component: RegisterComponent,
     canActivate: [UnauthGuard],
+  },
+  { path: 'post', component: PostComponent, canActivate: [AuthGuard] },
+  { path: 'topic', component: TopicComponent, canActivate: [AuthGuard] },
+  { path: 'user', component: UserComponent, canActivate: [AuthGuard] },
+  {
+    path: 'createpost',
+    component: CreatePostComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'post/:id',
+    component: PostDetailComponent,
+    canActivate: [AuthGuard],
   },
 ];
 
